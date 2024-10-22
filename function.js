24d8c96339c00aba0c6e93cad535c26d554fd038
Devcmar <111069142+Devcmar@users.noreply.github.com>
2024-10-22 15:33:08 +0200

    Update function.js

--- function.js
+++ function.js
@@ -319,36 +319,3 @@ function formatNumbersEuros(number) {
 function formatNumbers(number) {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
-
-function addSearchIconToMainSelect2(select2Id) {
-  var select2Container = document.querySelector(`#${select2Id} + .select2-container .select2-selection`);
-
-  if (select2Container && !select2Container.parentNode.querySelector('.search-icon')) {
-    
-    var searchIconSpan = document.createElement('span');
-    searchIconSpan.classList.add('search-icon');
-    searchIconSpan.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256" style="max-width:20px;max-height:20px;transform: translateX(-35px);">
-        <g fill="#e94b3c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
-          <g transform="scale(8.53333,8.53333)">
-            <path d="M13,3c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c2.39651,0 4.59738,-0.85101 6.32227,-2.26367l5.9707,5.9707c0.25082,0.26124 0.62327,0.36648 0.97371,0.27512c0.35044,-0.09136 0.62411,-0.36503 0.71547,-0.71547c0.09136,-0.35044 -0.01388,-0.72289 -0.27512,-0.97371l-5.9707,-5.9707c1.41266,-1.72488 2.26367,-3.92576 2.26367,-6.32227c0,-5.511 -4.489,-10 -10,-10zM13,5c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8z"></path>
-          </g>
-        </g>
-      </svg>`;
-    
-    select2Container.appendChild(searchIconSpan);
-  }
-}
-
-$('#secteur').select2({
-  placeholder: 'Sélectionner un secteur',
-  allowClear: true
-});
-
-$('#activite').select2({
-  placeholder: 'Sélectionner une activité',
-  allowClear: true
-});
-
-addSearchIconToMainSelect2('secteur');
-addSearchIconToMainSelect2('activite');
