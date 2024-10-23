f6ba3b97cd3a9e55efbeb6f238e0c9a2e7b0dd2a
Devcmar <111069142+Devcmar@users.noreply.github.com>
2024-10-23 11:51:09 +0200

    Update function.js

--- function.js
+++ function.js
@@ -324,9 +324,10 @@ document.addEventListener("DOMContentLoaded", function () {
     function appendSearchIcon() {
         const searchIconSpan = document.createElement('span');
         searchIconSpan.classList.add('search-icon');
+
         searchIconSpan.innerHTML = `
-            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 256 256" style="max-width:20px;max-height:20px;transform: translateX(-35px);">
-                <g fill="#e94b3c">
+            <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0,0,256,256" style="max-width:20px;max-height:20px;transform: translateX(-35px);">
+                <g fill="#e94b3c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
                     <g transform="scale(8.53333,8.53333)">
                         <path d="M13,3c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c2.39651,0 4.59738,-0.85101 6.32227,-2.26367l5.9707,5.9707c0.25082,0.26124 0.62327,0.36648 0.97371,0.27512c0.35044,-0.09136 0.62411,-0.36503 0.71547,-0.71547c0.09136,-0.35044 -0.01388,-0.72289 -0.27512,-0.97371l-5.9707,-5.9707c1.41266,-1.72488 2.26367,-3.92576 2.26367,-6.32227c0,-5.511 -4.489,-10 -10,-10zM13,5c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8z"></path>
                     </g>
@@ -337,15 +338,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const secteurSearchInput = document.querySelector('#secteur + span .select2-search__field');
         const activiteSearchInput = document.querySelector('#activite + span .select2-search__field');
 
-        const secteurSelect = document.getElementById('secteur');
-        const activiteSelect = document.getElementById('activite');
-
-        if (secteurSelect) {
-            secteurSelect.parentNode.querySelector('span').appendChild(searchIconSpan.cloneNode(true));
+        if (secteurSearchInput) {
+            secteurSearchInput.parentNode.appendChild(searchIconSpan.cloneNode(true));
+        } else {
+            console.error('Secteur search input not found.');
         }
 
-        if (activiteSelect) {
-            activiteSelect.parentNode.querySelector('span').appendChild(searchIconSpan.cloneNode(true));
+        if (activiteSearchInput) {
+            activiteSearchInput.parentNode.appendChild(searchIconSpan.cloneNode(true));
+        } else {
+            console.error('Activite search input not found.');
         }
     }
 
@@ -362,7 +364,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     searchIconCity.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 256 256" style="max-width:20px;max-height:20px;">
-            <g fill="#e94b3c">
+            <g fill="#e94b3c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10">
                 <g transform="scale(8.53333,8.53333)">
                     <path d="M13,3c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c2.39651,0 4.59738,-0.85101 6.32227,-2.26367l5.9707,5.9707c0.25082,0.26124 0.62327,0.36648 0.97371,0.27512c0.35044,-0.09136 0.62411,-0.36503 0.71547,-0.71547c0.09136,-0.35044 -0.01388,-0.72289 -0.27512,-0.97371l-5.9707,-5.9707c1.41266,-1.72488 2.26367,-3.92576 2.26367,-6.32227c0,-5.511 -4.489,-10 -10,-10zM13,5c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8z"></path>
                 </g>
@@ -372,7 +374,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     searchIconAnnounce.innerHTML = `
         <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 256 256" style="max-width:20px;max-height:20px;">
-            <g fill="#e94b3c">
+            <g fill="#e94b3c" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10">
                 <g transform="scale(8.53333,8.53333)">
                     <path d="M13,3c-5.511,0 -10,4.489 -10,10c0,5.511 4.489,10 10,10c2.39651,0 4.59738,-0.85101 6.32227,-2.26367l5.9707,5.9707c0.25082,0.26124 0.62327,0.36648 0.97371,0.27512c0.35044,-0.09136 0.62411,-0.36503 0.71547,-0.71547c0.09136,-0.35044 -0.01388,-0.72289 -0.27512,-0.97371l-5.9707,-5.9707c1.41266,-1.72488 2.26367,-3.92576 2.26367,-6.32227c0,-5.511 -4.489,-10 -10,-10zM13,5c4.43012,0 8,3.56988 8,8c0,4.43012 -3.56988,8 -8,8c-4.43012,0 -8,-3.56988 -8,-8c0,-4.43012 3.56988,-8 8,-8z"></path>
                 </g>
@@ -400,11 +402,18 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    [secteurSelect, activiteSelect].forEach(select => {
-        if (select) {
-            const icon = select.nextElementSibling;
-            select.addEventListener("change", function () {
-                toggleIconVisibility(select, icon);
+    ['secteur', 'activite'].forEach(function (selectId) {
+        const selectInput = document.querySelector(`#${selectId} + span .select2-search__field`);
+        if (selectInput) {
+            const icon = selectInput.parentNode.nextElementSibling;
+
+            selectInput.addEventListener("input", function () {
+                toggleIconVisibility(selectInput, icon);
+            });
+
+            const parentSelect = document.querySelector(`#${selectId}`);
+            parentSelect.addEventListener("change", function (e) {
+                toggleIconVisibility(selectInput, icon);
             });
         }
     });
